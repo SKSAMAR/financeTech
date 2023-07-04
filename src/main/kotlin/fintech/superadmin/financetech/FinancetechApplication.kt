4e4ef5309566b737d2785515cc8ecc76778b25c8
Samar <iamalsksamar@gmail.com>
2023-07-04 12:51:00 +0530

    Initial commit

---
+++ src/main/kotlin/fintech/superadmin/financetech/FinancetechApplication.kt
@@ -0,0 +1,11 @@
+package fintech.superadmin.financetech
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancetechApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinancetechApplication>(*args)
+}
---
+++ src/test/kotlin/fintech/superadmin/financetech/FinancetechApplicationTests.kt
@@ -0,0 +1,13 @@
+package fintech.superadmin.financetech
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinancetechApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
